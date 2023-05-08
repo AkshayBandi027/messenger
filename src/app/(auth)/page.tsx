6ef9ae55ed694c38b710b10a55d1f7f10bd44911
akshaybandi271 <akshaybandi271@gmail.com>
2023-05-08 22:52:33 +0530

    first commit

---
+++ src/app/(auth)/page.tsx
@@ -0,0 +1,7 @@
+export default function AuthenticationPage() {
+ return (
+    // AuthForm components that has input and error handling.
+    // AuthButtons which enables authentication with github and google.
+    // state toggling between register and login.
+ )
+}
